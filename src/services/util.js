3e1e202600a9ade62b1a9624beecced7c562391f
Daniel Mihalachi <dmihalachi@ca.imsbrogan.com>
2022-04-16 19:33:26 -0400

    more unit testing

--- src/services/util.js
+++ src/services/util.js
@@ -20,10 +20,14 @@ export const stringToBytes = (text) => {
 
 
 export const byteToBits = (byte) => {
-	const result = [];
-	for (let position = 0; position < 8; position++) {
-		result.push(byte & 0x01);
-		byte = byte >> 1;
-	}
-	return result;
+	return [
+		(byte >>> 7) & 1,
+		(byte >>> 6) & 1,
+		(byte >>> 5) & 1,
+		(byte >>> 4) & 1,
+		(byte >>> 3) & 1,
+		(byte >>> 2) & 1,
+		(byte >>> 1) & 1,
+		byte & 1,
+	];
 };
--- src/services/util.test.js
+++ src/services/util.test.js
@@ -1,6 +1,32 @@
-import { byteToBits } from './util';
+import { stringToBytes, byteToBits } from './util';
+
+
+test('stringToBytes', () => {
+	const bytes = stringToBytes('AB');
+	const expected = Uint8Array.from([65,66]);
+	expect(bytes.toString()).toEqual(expected.toString());	// Jest sees `expected` as an object rather than an array
+});
+
 
 test('byteToBits - all ones', () => {
 	const bits = byteToBits(255);
 	expect(bits).toEqual([1,1,1,1,1,1,1,1]);
+});
+
+
+test('byteToBits - all zeros', () => {
+	const bits = byteToBits(0);
+	expect(bits).toEqual([0,0,0,0,0,0,0,0]);
+});
+
+
+test('byteToBits - ones and zeros', () => {
+	const bits = byteToBits(parseInt('10101010', 2));
+	expect(bits).toEqual([1,0,1,0,1,0,1,0]);
+});
+
+
+test('byteToBits - zeros and ones', () => {
+	const bits = byteToBits(parseInt('01010101', 2));
+	expect(bits).toEqual([0,1,0,1,0,1,0,1]);
 });
