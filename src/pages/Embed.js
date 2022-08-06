5cee5177549e27d8c231306180cbaa946b8704b0
Daniel Mihalachi <dmihalachi@ca.imsbrogan.com>
2022-08-06 19:30:34 -0400

    layout fine tuning

--- src/pages/Embed.js
+++ src/pages/Embed.js
@@ -1,11 +1,10 @@
 import { FilePicker, FormWrapper } from '../widgets';
 import { useState } from 'react';
-import Form from 'react-bootstrap/Form';
+import { FormGroup, Form } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 
 import { stringToBytes, downloadBlob, embedMessage } from '../services/util';
 import { encrypt } from '../services/cipher';
-import { FormGroup } from 'react-bootstrap';
 
 
 const Embed = () => {
@@ -33,6 +32,7 @@ const Embed = () => {
 			<div className="page-wrapper">
 				<h1 className="mb-3">Embed Text into PNG</h1>
 				<p>This page encrypts and then embeds a message into a PNG file by making imperceptible changes to some pixels.</p>
+				<p>Once the message is embedded, you will be prompted to save the altered PNG file.</p>
 				<p>To extract the embeded message, go to the <a href="/extract">Extract</a> page.</p>
 
 				<FormWrapper>
