aab6a02966c5dfe433ae04784771806a19debede
Daniel Mihalachi <dmihalachi@ca.imsbrogan.com>
2022-08-19 19:45:30 -0400

    nicer navbar

--- src/widgets/MainMenu.js
+++ src/widgets/MainMenu.js
@@ -3,13 +3,18 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 
 const MainMenu = () => {
 	return (
-		<Navbar bg="light" expand="lg">
+		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
 			<Container>
 				<Navbar.Brand href="/">PNG Text</Navbar.Brand>
-				<Nav className="me-auto">
-					<Nav.Link href="/">Embed</Nav.Link>
-					<Nav.Link href="/extract">Extract</Nav.Link>
-				</Nav>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="me-auto">
+					</Nav>
+					<Nav>
+						<Nav.Link href="/">Embed</Nav.Link>
+						<Nav.Link href="/extract">Extract</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
 			</Container>
 		</Navbar>
 	);
