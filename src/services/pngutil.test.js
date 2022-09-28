8f0b9815caaf28fa2e545d86fb6ec476085d63bc
Daniel Mihalachi <dmihalachi@ca.imsbrogan.com>
2022-09-28 10:13:41 -0400

    png compression validation

--- src/services/pngutil.test.js
+++ src/services/pngutil.test.js
@@ -1,5 +1,5 @@
 import {
-	getPrefix, getSuffix, getWrappedBytes, writeToPngData
+	getPrefix, getSuffix, getWrappedBytes, writeToPngData, pngToBuffer, arrayBufferToPng
 } from './pngutil';
 
 import {
@@ -9,18 +9,46 @@ import { decrypt } from './cipher';
 import { bytesToString } from './stringlib';
 import { getLsbWithMask, findValues } from './arraylib';
 import { bitsToBytes, bytesToBits } from './bitlib';
+import { PNG } from 'pngjs';
 
 
 /**
- * 
- * @param {number} length 
+ * Create a Uint8Array of specified size.
+ * @param {number} pixelCount The number of pixels in this PNG.
  */
-export const makePngData = (length) => {
+const makePngData = (pixelCount) => {
 	return Uint8Array.from(
-		{ length },
+		{ length: pixelCount * 4 },
 		(_, i) => i % 256
 	);
 };
+
+
+const makePng = (width, height) => {
+	const png = new PNG({
+		width,
+		height
+	});
+	png.data = makePngData(width * height);
+	return png;
+};
+
+
+
+test('pngToBuffer & arrayBufferToPng', async () => {
+	const png1 = makePng(5, 2);
+	const buffer = pngToBuffer(png1);
+	expect(buffer).toBeTruthy();
+	const png2 = await arrayBufferToPng(buffer);
+	expect(png2).toBeTruthy();
+	expect(png2.width).toEqual(png1.width);
+	expect(png2.height).toEqual(png1.height);
+	expect(png2.data.length).toEqual(png1.data.length);
+	for (let index = 0; index < png1.length; index++) {
+		expect(png2.data[index]).toEqual(png1.data[index]);
+	}
+});
+
 
 
 test('getPrefix', () => {
@@ -58,7 +86,7 @@ test('writeToPngData', () => {
 	const password = 'Invent0r';
 	const wrappedBytes = getWrappedBytes(text, password);
 	const pixelsNeeded = Math.ceil(wrappedBytes.length * 8 / 3);
-	const pngData = makePngData(pixelsNeeded * 4);
+	const pngData = makePngData(pixelsNeeded);
 	writeToPngData(pngData, text, password);
 	const bits = getLsbWithMask(pngData, [1, 1, 1, 0]);
 	// find the prefix
