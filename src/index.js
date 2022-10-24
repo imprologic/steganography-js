8ae748b354631bab7026b95a26b4524f8114fc24
Daniel Mihalachi <dmihalachi@ca.imsbrogan.com>
2022-10-24 16:32:55 -0400

    WIP commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
@@ -16,5 +17,7 @@ root.render(
     <App />
   </StrictMode>,
 );
+
+serviceWorkerRegistration.register();
 
 
